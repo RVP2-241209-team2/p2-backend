f6251e6288cd03d183b201e349c760e8fea96573
Tachnyan <90418666+Tachnyan@users.noreply.github.com>
2024-12-27 16:51:24 -0600

    Moving to match restructure

---
+++ src/main/java/com/rvp2/server/ServerApplication.java
@@ -0,0 +1,17 @@
+package com.rvp2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EntityScan("com.rvp2.shoply.models")
+@ComponentScan("com.rvp2.shoply")
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
