ce763da5f2f63865fc48ec228ca63ee176ebddd0
Tachnyan <90418666+Tachnyan@users.noreply.github.com>
2025-01-03 11:59:02 -0600

    Added getters and setters to registration DTOs

--- src/main/java/com/revature/shoply/registration/dto/UserRegistrationRequestDTO.java
+++ src/main/java/com/revature/shoply/registration/dto/UserRegistrationRequestDTO.java
@@ -4,18 +4,27 @@ import com.revature.shoply.models.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserRegistrationRequestDTO {
+    @Getter @Setter
     private String username;
+    @Getter @Setter
     private String firstName;
+    @Getter @Setter
     private String lastName;
+    @Getter @Setter
     private String email;
+    @Getter @Setter
     private String password;
+    @Getter @Setter
     private String phoneNumber;
+    @Getter @Setter
     private UserRole role;
 }
--- src/main/java/com/revature/shoply/registration/dto/UserRegistrationResponseDTO.java
+++ src/main/java/com/revature/shoply/registration/dto/UserRegistrationResponseDTO.java
@@ -3,7 +3,9 @@ import com.revature.shoply.models.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
@@ -12,11 +14,18 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class UserRegistrationResponseDTO {
+    @Getter @Setter
     private UUID id;
+    @Getter @Setter
     private String username;
+    @Getter @Setter
     private String firstName;
+    @Getter @Setter
     private String lastName;
+    @Getter @Setter
     private String email;
+    @Getter @Setter
     private String phoneNumber;
+    @Getter @Setter
     private UserRole role;
 }
