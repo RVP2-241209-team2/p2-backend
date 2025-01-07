e7dc2caa8010810147256f5503d33ceadbb3dfd9
Tachnyan <90418666+Tachnyan@users.noreply.github.com>
2025-01-07 14:03:47 -0600

    File rename

--- src/main/java/com/revature/shoply/product/repository/ProductDAO.java
+++ src/main/java/com/revature/shoply/product/repository/ProductDAO.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductDAO extends JpaRepository<Product, UUID> {
 
     public Optional<Product> findByName(String name);
 }
